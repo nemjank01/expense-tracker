5ead408e431556168eae18dfb4a36eb4f867de47
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-03-23 14:16:52 +0100

    add folder structure and connect do mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
 
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //DB
+connectDB();
 
 //ROUTES
 app.get("/", (req, res) => {
