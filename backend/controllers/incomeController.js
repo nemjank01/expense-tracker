71e99ebc45d49ce34c9d15519e6f8b44e6676b42
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-03-24 11:21:09 +0100

    add addIncome controller function

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,37 @@
+import Income from "../models/incomeModel.js";
+
+export async function addIncome(req, res) {
+  const userId = req.user._id;
+  const { description, category, amount, date } = req.body;
+
+  try {
+    if (!description || !category || !amount || !date) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required.",
+      });
+    }
+
+    const newIncome = new Income({
+      userId,
+      description,
+      category,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Income created successfully!",
+      data: newIncome,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Server error.",
+    });
+  }
+}
